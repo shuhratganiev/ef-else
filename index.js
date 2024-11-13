69a0386b9140760a99838ca9156b391784340546
shuhratganiev <shganiev009@gmail.com>
2024-11-13 18:08:21 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// let summa = 1000
+// let banan = 1200
+// let alma = 500
+// console.log("bizdegi summa", summa );
+// console.log("banan summasi", banan );
+// console.log("alma summasi", alma );
+
+
+// if (summa >=banan) {
+//     console.log("summa bananga jetedi");
+  
+    
+    
+// }  else{
+//         console.log("summa bananga jetpeydi");
+        
+//     }
+//     if (summa >=alma) {
+//         console.log("summa almaga jetedi");
+        
+        
+//     }else console.log("summa almaga jetpeydi");
+    
+// let san =33
+// console.log("san", san);
+
+// if (san % 2 === 0) {
+//     console.log("jup sann");
+   
+// }else{
+//     console.log("san taqq");
+    
+// }
+// 1
+// let san = 23
+// if (san<0) {
+//     console.log(san+1);
+// }else{
+//     console.log(san);
+// }
+// 2
+// let san = 23
+// if (san<0) {
+//     console.log(san+1);
+// }else{
+//     console.log(san-2);
+// }
+//3
+
+
